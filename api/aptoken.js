6a74f75d026151d60c5168066a7e7a970ac87070
pp-016 <hanjiayu016@gmail.com>
2025-06-19 15:43:29 +0800

    Create aptoken.js

---
+++ api/aptoken.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const body = {
+    grant_type: 'client_credentials',
+    client_id: process.env.ALIYUN_ACCESS_KEY_ID,
+    client_secret: process.env.ALIYUN_ACCESS_KEY_SECRET
+  }
+
+  const resp = await fetch('https://nls-meta.cn-shanghai.aliyuncs.com/v1/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  })
+
+  const data = await resp.json()
+  res.status(200).json(data)
+}
